70b365102de4b38479ee6c9b9101312a7731cfc8
Callum Whyte <hey@callumwhyte.com>
2019-02-03 02:43:41 +0000

    Add basic MNTP converter inheriting from Umbraco Core MNTP converter

---
+++ src/Our.Umbraco.SuperValueConverters/ValueConverters/MultiNodeTreePickerValueConverter.cs
@@ -0,0 +1,13 @@
+﻿using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web.PropertyEditors.ValueConverters;
+
+namespace Our.Umbraco.SuperValueConverters.ValueConverters
+{
+    public class MultiNodeTreePickerValueConverter : MultiNodeTreePickerPropertyConverter
+    {
+        public override object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return base.ConvertDataToSource(propertyType, source, preview);
+        }
+    }
+}
