9260b46b864f6f762902b4117c0b08bb25fa1a89
Callum Whyte <hey@callumwhyte.com>
2019-02-03 02:52:08 +0000

    Determining converter return type from picker settings allowed doctypes and maximum items

--- src/Our.Umbraco.SuperValueConverters/ValueConverters/MultiNodeTreePickerValueConverter.cs
+++ src/Our.Umbraco.SuperValueConverters/ValueConverters/MultiNodeTreePickerValueConverter.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Our.Umbraco.SuperValueConverters.Helpers;
+using Umbraco.Core.Models;
 using Umbraco.Core.Models.PublishedContent;
 using Umbraco.Core.PropertyEditors;
 using Umbraco.Web.PropertyEditors.ValueConverters;
@@ -32,7 +36,26 @@ namespace Our.Umbraco.SuperValueConverters.ValueConverters
 
         public Type GetPropertyValueType(PublishedPropertyType propertyType)
         {
-            throw new NotImplementedException();
+            var pickerSettings = DataTypeHelper.GetPickerSettings(propertyType.DataTypeId);
+
+            var modelType = typeof(IPublishedContent);
+
+            if (pickerSettings.AllowedDoctypes.Count() == 1)
+            {
+                var foundType = TypeHelper.GetType(pickerSettings.AllowedDoctypes.FirstOrDefault());
+
+                if (foundType != null)
+                {
+                    modelType = foundType;
+                }
+            }
+
+            if (pickerSettings.AllowsMultiple() == true)
+            {
+                return typeof(IEnumerable<>).MakeGenericType(modelType);
+            }
+
+            return modelType;
         }
 
         public override object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
