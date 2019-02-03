55783fa72ac0cfd8a06b787cd9df6bd1b951d4a4
Callum Whyte <hey@callumwhyte.com>
2019-02-03 02:48:49 +0000

    Implement IPropertyValueConverterMeta interface on MNTP converter

--- src/Our.Umbraco.SuperValueConverters/ValueConverters/MultiNodeTreePickerValueConverter.cs
+++ src/Our.Umbraco.SuperValueConverters/ValueConverters/MultiNodeTreePickerValueConverter.cs
@@ -1,10 +1,40 @@
-﻿using Umbraco.Core.Models.PublishedContent;
+﻿using System;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
 using Umbraco.Web.PropertyEditors.ValueConverters;
 
 namespace Our.Umbraco.SuperValueConverters.ValueConverters
 {
-    public class MultiNodeTreePickerValueConverter : MultiNodeTreePickerPropertyConverter
+    public class MultiNodeTreePickerValueConverter : MultiNodeTreePickerPropertyConverter, IPropertyValueConverterMeta
     {
+        public PropertyCacheLevel GetPropertyCacheLevel(PublishedPropertyType propertyType, PropertyCacheValue cacheValue)
+        {
+            PropertyCacheLevel returnLevel;
+
+            switch (cacheValue)
+            {
+                case PropertyCacheValue.Object:
+                    returnLevel = PropertyCacheLevel.ContentCache;
+                    break;
+                case PropertyCacheValue.Source:
+                    returnLevel = PropertyCacheLevel.Content;
+                    break;
+                case PropertyCacheValue.XPath:
+                    returnLevel = PropertyCacheLevel.Content;
+                    break;
+                default:
+                    returnLevel = PropertyCacheLevel.None;
+                    break;
+            }
+
+            return returnLevel;
+        }
+
+        public Type GetPropertyValueType(PublishedPropertyType propertyType)
+        {
+            throw new NotImplementedException();
+        }
+
         public override object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
         {
             return base.ConvertDataToSource(propertyType, source, preview);
