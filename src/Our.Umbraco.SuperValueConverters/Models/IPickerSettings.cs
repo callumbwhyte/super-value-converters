a451d2229ffb20d3dd38361271bdfb5c5e9ae0be
Callum Whyte <hey@callumwhyte.com>
2019-02-04 17:26:10 +0000

    Adding individual settings class per converter type

---
+++ src/Our.Umbraco.SuperValueConverters/Models/IPickerSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Our.Umbraco.SuperValueConverters.Models
+{
+    public interface IPickerSettings
+    {
+        string[] AllowedDoctypes { get; set; }
+        int MaxItems { get; set; }
+        bool AllowsMultiple();
+    }
+}
