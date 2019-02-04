891d103b0e73ccc03e710ef5a875e6653784a743
Callum Whyte <hey@callumwhyte.com>
2019-02-04 16:46:50 +0000

    Introducing ModelsBuilder helper to get configuration

---
+++ src/Our.Umbraco.SuperValueConverters/Helpers/ModelsBuilderHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Configuration;
+
+namespace Our.Umbraco.SuperValueConverters.Helpers
+{
+    public class ModelsBuilderHelper
+    {
+        private const string Prefix = "Umbraco.ModelsBuilder.";
+
+        public static bool IsEnabled()
+        {
+            var value = ConfigurationManager.AppSettings[Prefix + "Enable"];
+
+            return Convert.ToBoolean(value);
+        }
+
+        public static string GetNamespace()
+        {
+            var value = ConfigurationManager.AppSettings[Prefix + "ModelsNamespace"];
+
+            if (string.IsNullOrWhiteSpace(value) == false)
+            {
+                return value;
+            }
+
+            return "Umbraco.Web.PublishedContentModels";
+        }
+    }
+}
