524219d08f25c7e059e64ff8eeb590ac0414e91c
Callum Whyte <hey@callumwhyte.com>
2019-02-04 18:58:50 +0000

    Adding code to process mapping prevalues to IPickerSettings classes

---
+++ src/Our.Umbraco.SuperValueConverters/Helpers/PreValueAttributeHelper.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Our.Umbraco.SuperValueConverters.Attributes;
+using Our.Umbraco.SuperValueConverters.Models;
+
+namespace Our.Umbraco.SuperValueConverters.Helpers
+{
+    public class PreValueAttributeHelper
+    {
+        public static IPickerSettings Map(IPickerSettings pickerSettings, IDictionary<string, string> preValues)
+        {
+            var properties = pickerSettings.GetType().GetProperties();
+
+            foreach (var property in properties)
+            {
+                var preValueProperty = property.GetCustomAttribute<PreValuePropertyAttribute>();
+
+                if (preValueProperty != null)
+                {
+                    var value = preValues[preValueProperty.Alias];
+
+                    if (property.PropertyType == typeof(bool))
+                    {
+                        property.SetValue(pickerSettings, ConvertToBoolean(value));
+                    }
+
+                    if (property.PropertyType == typeof(int))
+                    {
+                        property.SetValue(pickerSettings, ConvertToInt(value));
+                    }
+
+                    if (property.PropertyType == typeof(string[]))
+                    {
+                        property.SetValue(pickerSettings, ConvertToStringArray(value));
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private static bool ConvertToBoolean(string input)
+        {
+            bool value = false;
+
+            bool.TryParse(input, out value);
+
+            return value;
+        }
+
+        private static int ConvertToInt(string input)
+        {
+            int value = 0;
+
+            int.TryParse(input, out value);
+
+            return value;
+        }
+
+        private static string[] ConvertToStringArray(string input, char separator = ',')
+        {
+            return input.Replace(" ", "").Split(separator);
+        }
+    }
+}
