b9ad5092697c6970e75c61f8c8a9f15a320b8320
Callum Whyte <hey@callumwhyte.com>
2019-02-04 04:58:04 +0000

    Adding parameter for finding types within a given namespace in TypeHelper class

--- src/Our.Umbraco.SuperValueConverters/Helpers/TypeHelper.cs
+++ src/Our.Umbraco.SuperValueConverters/Helpers/TypeHelper.cs
@@ -1,20 +1,27 @@
 ﻿using System;
 using System.Collections;
+using System.Linq;
 
 namespace Our.Umbraco.SuperValueConverters.Helpers
 {
     public class TypeHelper
     {
-        public static Type GetType(string typeName)
+        public static Type GetType(string typeName, string namespaceName = null)
         {
-            foreach (var assembley in AppDomain.CurrentDomain.GetAssemblies())
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(x => x.GetTypes())
+                .Where(x => x.IsClass);
+
+            if (string.IsNullOrEmpty(namespaceName) == false)
             {
-                foreach (var type in assembley.GetTypes())
+                types = types.Where(x => x.Namespace.Equals(namespaceName, StringComparison.InvariantCultureIgnoreCase));
+            }
+
+            foreach (var type in types)
+            {
+                if (type.Name.Equals(typeName, StringComparison.InvariantCultureIgnoreCase))
                 {
-                    if (type.Name.Equals(typeName, StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        return type;
-                    }
+                    return type;
                 }
             }
 
