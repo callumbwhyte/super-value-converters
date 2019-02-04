976be6634377c082cc29e6325d20895d7f72fcab
Callum Whyte <hey@callumwhyte.com>
2019-02-04 17:08:25 +0000

    Moving common duplicated value converter code into base class

---
+++ src/Our.Umbraco.SuperValueConverters/ValueConverters/BaseValueConverter.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Our.Umbraco.SuperValueConverters.Extensions;
+using Our.Umbraco.SuperValueConverters.Helpers;
+using Our.Umbraco.SuperValueConverters.Models;
+using Umbraco.Core.Models;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+
+namespace Our.Umbraco.SuperValueConverters.ValueConverters
+{
+    public class BaseValueConverter
+    {
+        public static PropertyCacheLevel GetPropertyCacheLevel(PublishedPropertyType propertyType, PropertyCacheValue cacheValue)
+        {
+            PropertyCacheLevel returnLevel;
+
+            switch (cacheValue)
+            {
+                case PropertyCacheValue.Object:
+                    returnLevel = PropertyCacheLevel.ContentCache;
+                    break;
+                case PropertyCacheValue.Source:
+                    returnLevel = PropertyCacheLevel.Content;
+                    break;
+                case PropertyCacheValue.XPath:
+                    returnLevel = PropertyCacheLevel.Content;
+                    break;
+                default:
+                    returnLevel = PropertyCacheLevel.None;
+                    break;
+            }
+
+            return returnLevel;
+        }
+
+        public static Type GetPropertyValueType(PublishedPropertyType propertyType, IPickerSettings pickerSettings)
+        {
+            var modelType = typeof(IPublishedContent);
+
+            if (pickerSettings.AllowedDoctypes.Count() == 1
+                && ModelsBuilderHelper.IsEnabled() == true)
+            {
+                var modelsNamespace = ModelsBuilderHelper.GetNamespace();
+
+                var foundType = TypeHelper.GetType(pickerSettings.AllowedDoctypes.FirstOrDefault(), modelsNamespace);
+
+                if (foundType != null)
+                {
+                    modelType = foundType;
+                }
+            }
+
+            if (pickerSettings.AllowsMultiple() == true)
+            {
+                return typeof(IEnumerable<>).MakeGenericType(modelType);
+            }
+
+            return modelType;
+        }
+
+        public static object ConvertSourceToObject(PublishedPropertyType propertyType, IEnumerable<IPublishedContent> value)
+        {
+            var clrType = propertyType.ClrType;
+
+            bool allowsMultiple = TypeHelper.IsIEnumerable(clrType);
+
+            var innerType = allowsMultiple ? TypeHelper.GetInnerType(clrType) : clrType;
+
+            var list = innerType == typeof(IPublishedContent)
+                ? new List<IPublishedContent>()
+                : TypeHelper.CreateListOfType(innerType);
+
+            if (value != null)
+            {
+                foreach (var item in value)
+                {
+                    var itemType = item.GetType();
+
+                    if (itemType == innerType)
+                    {
+                        list.Add(item);
+                    }
+                    else if (innerType == typeof(IPublishedContent)
+                        && TypeHelper.IsIPublishedContent(itemType) == true)
+                    {
+                        list.Add(item);
+                    }
+                }
+            }
+
+            return allowsMultiple == true ? list : list.FirstOrNull();
+        }
+    }
+}
--- src/Our.Umbraco.SuperValueConverters/ValueConverters/MultiNodeTreePickerValueConverter.cs
+++ src/Our.Umbraco.SuperValueConverters/ValueConverters/MultiNodeTreePickerValueConverter.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using Our.Umbraco.SuperValueConverters.Extensions;
 using Our.Umbraco.SuperValueConverters.Helpers;
 using Umbraco.Core.Models;
 using Umbraco.Core.Models.PublishedContent;
@@ -14,87 +12,21 @@ namespace Our.Umbraco.SuperValueConverters.ValueConverters
     {
         public PropertyCacheLevel GetPropertyCacheLevel(PublishedPropertyType propertyType, PropertyCacheValue cacheValue)
         {
-            PropertyCacheLevel returnLevel;
-
-            switch (cacheValue)
-            {
-                case PropertyCacheValue.Object:
-                    returnLevel = PropertyCacheLevel.ContentCache;
-                    break;
-                case PropertyCacheValue.Source:
-                    returnLevel = PropertyCacheLevel.Content;
-                    break;
-                case PropertyCacheValue.XPath:
-                    returnLevel = PropertyCacheLevel.Content;
-                    break;
-                default:
-                    returnLevel = PropertyCacheLevel.None;
-                    break;
-            }
-
-            return returnLevel;
+            return BaseValueConverter.GetPropertyCacheLevel(propertyType, cacheValue);
         }
 
         public Type GetPropertyValueType(PublishedPropertyType propertyType)
         {
             var pickerSettings = DataTypeHelper.GetPickerSettings(propertyType.DataTypeId);
 
-            var modelType = typeof(IPublishedContent);
-
-            if (pickerSettings.AllowedDoctypes.Count() == 1
-                && ModelsBuilderHelper.IsEnabled() == true)
-            {
-                var modelsNamespace = ModelsBuilderHelper.GetNamespace();
-
-                var foundType = TypeHelper.GetType(pickerSettings.AllowedDoctypes.FirstOrDefault(), modelsNamespace);
-
-                if (foundType != null)
-                {
-                    modelType = foundType;
-                }
-            }
-
-            if (pickerSettings.AllowsMultiple() == true)
-            {
-                return typeof(IEnumerable<>).MakeGenericType(modelType);
-            }
-
-            return modelType;
+            return BaseValueConverter.GetPropertyValueType(propertyType, pickerSettings);
         }
 
         public override object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview)
         {
             var value = base.ConvertSourceToObject(propertyType, source, preview) as IEnumerable<IPublishedContent>;
 
-            var clrType = propertyType.ClrType;
-
-            bool allowsMultiple = TypeHelper.IsIEnumerable(clrType);
-
-            var innerType = allowsMultiple ? TypeHelper.GetInnerType(clrType) : clrType;
-
-            var list = innerType == typeof(IPublishedContent)
-                ? new List<IPublishedContent>()
-                : TypeHelper.CreateListOfType(innerType);
-
-            if (value != null)
-            {
-                foreach (var item in value)
-                {
-                    var itemType = item.GetType();
-
-                    if (itemType == innerType)
-                    {
-                        list.Add(item);
-                    }
-                    else if (innerType == typeof(IPublishedContent)
-                        && TypeHelper.IsIPublishedContent(itemType) == true)
-                    {
-                        list.Add(item);
-                    }
-                }
-            }
-
-            return allowsMultiple == true ? list : list.FirstOrNull();
+            return BaseValueConverter.ConvertSourceToObject(propertyType, value);
         }
     }
 }
