2eaf9c8e15b6cfcbf09576c476fd7181a3e21bb9
Callum Whyte <hey@callumwhyte.com>
2019-02-04 19:01:47 +0000

    Implementing existing custom mapping logic as PreValueFilters

---
+++ src/Our.Umbraco.SuperValueConverters/Attributes/MediaPickerMaxItemsFilterAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Our.Umbraco.SuperValueConverters.Attributes
+{
+    public class MediaPickerMaxItemsFilterAttribute : PreValueFilterAttribute
+    {
+        public override object Process(string input)
+        {
+            int value = 0;
+
+            if (input != "1")
+            {
+                value = 1;
+            }
+
+            return value;
+        }
+    }
+}
--- src/Our.Umbraco.SuperValueConverters/Models/MediaPickerSettings.cs
+++ src/Our.Umbraco.SuperValueConverters/Models/MediaPickerSettings.cs
@@ -5,9 +5,11 @@ namespace Our.Umbraco.SuperValueConverters.Models
     public class MediaPickerSettings : IPickerSettings
     {
         [PreValueProperty("onlyImages")]
+        [MediaPickerAllowedDoctypesFilter]
         public string[] AllowedDoctypes { get; set; }
 
         [PreValueProperty("multiPicker")]
+        [MediaPickerMaxItemsFilter]
         public int MaxItems { get; set; }
 
         public bool AllowsMultiple()
