5e711f02760ba2d5d8518929205304b68e94fb04
Callum Whyte <hey@callumwhyte.com>
2019-02-04 16:48:45 +0000

    Adding media picker converter

---
+++ src/Our.Umbraco.SuperValueConverters/ValueConverters/MediaPickerValueConverter.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Our.Umbraco.SuperValueConverters.Extensions;
+using Our.Umbraco.SuperValueConverters.Helpers;
+using Umbraco.Core.Models;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Web.PropertyEditors.ValueConverters;
+
+namespace Our.Umbraco.SuperValueConverters.ValueConverters
+{
+    public class MediaPickerValueConverter : MediaPickerPropertyConverter, IPropertyValueConverterMeta
+    {
+        public new PropertyCacheLevel GetPropertyCacheLevel(PublishedPropertyType propertyType, PropertyCacheValue cacheValue)
+        {
+            PropertyCacheLevel returnLevel;
+
+            switch (cacheValue)
+            {
+                case PropertyCacheValue.Object:
+                    returnLevel = PropertyCacheLevel.ContentCache;
+                    break;
+                case PropertyCacheValue.Source:
+                    returnLevel = PropertyCacheLevel.Content;
+                    break;
+                case PropertyCacheValue.XPath:
+                    returnLevel = PropertyCacheLevel.Content;
+                    break;
+                default:
+                    returnLevel = PropertyCacheLevel.None;
+                    break;
+            }
+
+            return returnLevel;
+        }
+
+        public new Type GetPropertyValueType(PublishedPropertyType propertyType)
+        {
+            var pickerSettings = DataTypeHelper.GetMediaPickerSettings(propertyType.DataTypeId);
+
+            var modelType = typeof(IPublishedContent);
+
+            if (pickerSettings.AllowedDoctypes.Count() == 1
+                && ModelsBuilderHelper.IsEnabled() == true)
+            {
+                var modelsNamespace = ModelsBuilderHelper.GetNamespace();
+
+                var foundType = TypeHelper.GetType(pickerSettings.AllowedDoctypes.FirstOrDefault(), modelsNamespace);
+
+                if (foundType != null)
+                {
+                    modelType = foundType;
+                }
+            }
+
+            if (pickerSettings.AllowsMultiple() == true)
+            {
+                return typeof(IEnumerable<>).MakeGenericType(modelType);
+            }
+
+            return modelType;
+        }
+
+        public override object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            var value = base.ConvertSourceToObject(propertyType, source, preview) as IEnumerable<IPublishedContent>;
+
+            var clrType = propertyType.ClrType;
+
+            bool allowsMultiple = TypeHelper.IsIEnumerable(clrType);
+
+            var innerType = allowsMultiple ? TypeHelper.GetInnerType(clrType) : clrType;
+
+            var list = innerType == typeof(IPublishedContent)
+                ? new List<IPublishedContent>()
+                : TypeHelper.CreateListOfType(innerType);
+
+            if (value != null)
+            {
+                foreach (var item in value)
+                {
+                    var itemType = item.GetType();
+
+                    if (itemType == innerType)
+                    {
+                        list.Add(item);
+                    }
+                    else if (innerType == typeof(IPublishedContent)
+                        && TypeHelper.IsIPublishedContent(itemType) == true)
+                    {
+                        list.Add(item);
+                    }
+                }
+            }
+
+            return allowsMultiple == true ? list : list.FirstOrNull();
+        }
+    }
+}
