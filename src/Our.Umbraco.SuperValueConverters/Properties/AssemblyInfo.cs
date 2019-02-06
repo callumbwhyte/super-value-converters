a9928e0b3ac8df6cb00950eea5fb5e34dd977e11
Callum Whyte <hey@callumwhyte.com>
2019-02-06 04:02:59 +0000

    Updating assembly description

--- src/Our.Umbraco.SuperValueConverters/Properties/AssemblyInfo.cs
+++ src/Our.Umbraco.SuperValueConverters/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Our.Umbraco.SuperValueConverters")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A collection of powerful property value converters for cleaner code in Umbraco 7.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Our.Umbraco.SuperValueConverters")]
