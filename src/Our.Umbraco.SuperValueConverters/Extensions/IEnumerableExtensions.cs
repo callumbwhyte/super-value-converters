fab47baa50e1673d235183db20a9e0d61c21c949
Callum Whyte <hey@callumwhyte.com>
2019-04-13 22:57:24 +0100

    Extension method to intersect multiple IEnumerables and get common values

---
+++ src/Our.Umbraco.SuperValueConverters/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Our.Umbraco.SuperValueConverters.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> IntersectMany<T>(this IEnumerable<IEnumerable<T>> values)
+        {
+            IEnumerable<T> intersection = null;
+
+            foreach (var value in values)
+            {
+                if (intersection == null)
+                {
+                    intersection = new List<T>(value);
+                }
+                else
+                {
+                    intersection.Intersect(value);
+                }
+            }
+
+            return intersection ?? Enumerable.Empty<T>();
+        }
+    }
+}
