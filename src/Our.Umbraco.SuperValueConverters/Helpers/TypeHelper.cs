76ed2df2d2b802f42aff08eb7c9227c647ba6a51
Callum Whyte <hey@callumwhyte.com>
2019-05-11 20:49:01 +0100

    Update reference to IPublishedContent

--- src/Our.Umbraco.SuperValueConverters/Helpers/TypeHelper.cs
+++ src/Our.Umbraco.SuperValueConverters/Helpers/TypeHelper.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Umbraco.Core.Models;
+using Umbraco.Core.Models.PublishedContent;
 
 namespace Our.Umbraco.SuperValueConverters.Helpers
 {
