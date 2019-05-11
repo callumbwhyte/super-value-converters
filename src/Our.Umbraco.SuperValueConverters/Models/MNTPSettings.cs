0f72bfcc52d8db4fd55f0c2c7d6e6a44659d8534
Callum Whyte <hey@callumwhyte.com>
2019-05-11 17:59:44 +0100

    Removing legacy prevalue mapper

--- src/Our.Umbraco.SuperValueConverters/Models/MNTPSettings.cs
+++ src/Our.Umbraco.SuperValueConverters/Models/MNTPSettings.cs
@@ -1,13 +1,11 @@
-﻿using Our.Umbraco.SuperValueConverters.PreValues.Attributes;
+﻿using System;
 
 namespace Our.Umbraco.SuperValueConverters.Models
 {
     public class MNTPSettings : IPickerSettings
     {
-        [PreValueProperty("filter")]
         public string[] AllowedDoctypes { get; set; }
 
-        [PreValueProperty("maxNumber")]
         public int MaxItems { get; set; }
 
         public bool AllowsMultiple()
--- src/Our.Umbraco.SuperValueConverters/Models/MediaPickerSettings.cs
+++ src/Our.Umbraco.SuperValueConverters/Models/MediaPickerSettings.cs
@@ -1,16 +1,11 @@
-﻿using Our.Umbraco.SuperValueConverters.Attributes;
-using Our.Umbraco.SuperValueConverters.PreValues.Attributes;
+﻿using System;
 
 namespace Our.Umbraco.SuperValueConverters.Models
 {
     public class MediaPickerSettings : IPickerSettings
     {
-        [PreValueProperty("onlyImages")]
-        [MediaPickerAllowedDoctypesFilter]
         public string[] AllowedDoctypes { get; set; }
 
-        [PreValueProperty("multiPicker")]
-        [MediaPickerMaxItemsFilter]
         public int MaxItems { get; set; }
 
         public bool AllowsMultiple()
