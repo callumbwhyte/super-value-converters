5f725087e4fc644ff6c8ce83ab332beccb9ee5bf
Callum Whyte <hey@callumwhyte.com>
2019-05-11 17:50:33 +0100

    Replacing ApplicationEventHandler with composers to override core value converters

---
+++ src/Our.Umbraco.SuperValueConverters/Startup/NestedContentComposer.cs
@@ -0,0 +1,17 @@
+﻿using Our.Umbraco.SuperValueConverters.ValueConverters;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+using Umbraco.Web.PropertyEditors.ValueConverters;
+
+namespace Our.Umbraco.SuperValueConverters.Startup
+{
+    public class NestedContentComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.PropertyValueConverters()
+                .Replace<NestedContentManyValueConverter, NestedContentValueConverter>()
+                .Replace<NestedContentSingleValueConverter, NestedContentValueConverter>();
+        }
+    }
+}
