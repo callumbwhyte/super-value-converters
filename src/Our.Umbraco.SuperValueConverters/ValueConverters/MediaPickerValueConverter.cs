f3507f21cea691d6e273cbd3ba11bfc78f4c8a06
Callum Whyte <hey@callumwhyte.com>
2019-07-21 18:43:38 +0100

    Converters now use new abstract base class

--- src/Our.Umbraco.SuperValueConverters/ValueConverters/MediaPickerValueConverter.cs
+++ src/Our.Umbraco.SuperValueConverters/ValueConverters/MediaPickerValueConverter.cs
@@ -1,36 +1,19 @@
-﻿using System;
-using Our.Umbraco.SuperValueConverters.Models;
+﻿using Our.Umbraco.SuperValueConverters.Models;
 using Umbraco.Core.Models.PublishedContent;
-using Umbraco.Core.PropertyEditors;
 using Umbraco.Web.PropertyEditors;
-using Umbraco.Web.PublishedCache;
 using Core = Umbraco.Web.PropertyEditors.ValueConverters;
 
 namespace Our.Umbraco.SuperValueConverters.ValueConverters
 {
-    public class MediaPickerValueConverter : Core.MediaPickerValueConverter
+    public class MediaPickerValueConverter : SuperValueConverterBase
     {
-        public MediaPickerValueConverter(IPublishedSnapshotAccessor publishedSnapshotAccessor)
-            : base(publishedSnapshotAccessor)
+        public MediaPickerValueConverter(Core.MediaPickerValueConverter baseValueConverter)
+            : base(baseValueConverter)
         {
 
         }
 
-        public override Type GetPropertyValueType(PublishedPropertyType propertyType)
-        {
-            var pickerSettings = GetSettings(propertyType);
-
-            return BaseValueConverter.GetPropertyValueType(propertyType, pickerSettings);
-        }
-
-        public override object ConvertIntermediateToObject(IPublishedElement owner, PublishedPropertyType propertyType, PropertyCacheLevel cacheLevel, object source, bool preview)
-        {
-            var value = base.ConvertIntermediateToObject(owner, propertyType, cacheLevel, source, preview);
-
-            return BaseValueConverter.ConvertIntermediateToObject(propertyType, value);
-        }
-
-        private IPickerSettings GetSettings(PublishedPropertyType propertyType)
+        public override IPickerSettings GetSettings(PublishedPropertyType propertyType)
         {
             var configuration = propertyType.DataType.ConfigurationAs<MediaPickerConfiguration>();
 
--- src/Our.Umbraco.SuperValueConverters/ValueConverters/NestedContentValueConverter.cs
+++ src/Our.Umbraco.SuperValueConverters/ValueConverters/NestedContentValueConverter.cs
@@ -1,29 +1,18 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using Our.Umbraco.SuperValueConverters.Models;
 using Umbraco.Core;
-using Umbraco.Core.Logging;
 using Umbraco.Core.Models.PublishedContent;
-using Umbraco.Core.PropertyEditors;
 using Umbraco.Web.PropertyEditors;
-using Umbraco.Web.PublishedCache;
 using Core = Umbraco.Web.PropertyEditors.ValueConverters;
 
 namespace Our.Umbraco.SuperValueConverters.ValueConverters
 {
-    public class NestedContentValueConverter : Core.NestedContentManyValueConverter
+    public class NestedContentValueConverter : SuperValueConverterBase
     {
-        public NestedContentValueConverter(IPublishedSnapshotAccessor publishedSnapshotAccessor, IPublishedModelFactory publishedModelFactory, IProfilingLogger proflog)
-            : base(publishedSnapshotAccessor, publishedModelFactory, proflog)
+        public NestedContentValueConverter(Core.NestedContentManyValueConverter baseValueConverter)
+            : base(baseValueConverter)
         {
 
-        }
-
-        public override Type GetPropertyValueType(PublishedPropertyType propertyType)
-        {
-            var pickerSettings = GetSettings(propertyType);
-
-            return BaseValueConverter.GetPropertyValueType(propertyType, pickerSettings);
         }
 
         public override bool IsConverter(PublishedPropertyType propertyType)
@@ -31,14 +20,7 @@ namespace Our.Umbraco.SuperValueConverters.ValueConverters
             return propertyType.EditorAlias.Equals(Constants.PropertyEditors.Aliases.NestedContent);
         }
 
-        public override object ConvertIntermediateToObject(IPublishedElement owner, PublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview)
-        {
-            var value = base.ConvertIntermediateToObject(owner, propertyType, referenceCacheLevel, inter, preview);
-
-            return BaseValueConverter.ConvertIntermediateToObject(propertyType, value);
-        }
-
-        private IPickerSettings GetSettings(PublishedPropertyType propertyType)
+        public override IPickerSettings GetSettings(PublishedPropertyType propertyType)
         {
             var configuration = propertyType.DataType.ConfigurationAs<NestedContentConfiguration>();
 
